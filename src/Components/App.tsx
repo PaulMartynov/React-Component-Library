c07c9807bf1d271b95035c228b5003b612f625f3
PaulMartynov <juanfen@yandex.ru>
2021-09-10 19:19:03 +0300

    add 1st component

---
+++ src/Components/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Header } from "./HeaderComponent/Header";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
---
+++ src/Components/HeaderComponent/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class Header extends React.Component<any, any> {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types,class-methods-use-this
+  render() {
+    return (
+      <>
+        <h1>TestHeaderComponent</h1>
+      </>
+    );
+  }
+}
